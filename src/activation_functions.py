d45c309b107a57ca43a27d8f465769ebd9fdcf15
Mostafa772 <47507394+Mostafa772@users.noreply.github.com>
2025-01-24 02:26:11 +0100

    Create activation_functions.py

---
+++ src/activation_functions.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        # Since we need to modify the orginial variable,
+        # let's make a copy of the values first
+        self.dinputs = dvalues.copy()
+        
+        # zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
+
+class Activation_Leaky_ReLU:
+    def forward(self, inputs, alpha=0.01):
+        self.inputs = inputs
+        self.output = np.where(inputs > 0, inputs, alpha * inputs)
+
+    def backward(self, dvalues, alpha=0.01):  # f′(x) = {1, α : if  x > 0 ; if x ≤ 0}
+        self.dinputs = dvalues.copy()
+        self.dinputs = np.where(self.inputs > 0, 1, alpha)
+        self.dinputs *= dvalues
+
+class Activation_Sigmoid:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = 1/(1 + np.exp(-inputs))
+
+    def backward(self, dvalues):  # f′(x) = σ(x)⋅(1−σ(x))
+        self.dinputs = dvalues.copy()
+        self.dinputs = self.output * (1 - self.output)
+        self.dinputs *= dvalues
+
+
+class Activation_Tanh:
+    def forward(self, inputs):
+        self.inputs = inputs
+        self.output = np.tanh(inputs)
+
+    def backward(self, dvalues):  # f′(x) = 1−tanh**2(x)
+        self.dinputs = dvalues.copy()
+        self.dinputs = 1 - (self.output)**2
+        self.dinputs *= dvalues
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        self.inputs = inputs
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+    def backward(self, dvalues):
+        self.dinputs = np.empty_like(dvalues)
+        
+        for index, (single_output, single_dvalues) in \
+                enumerate(zip(self.output, dvalues)):
+            single_output = single_output.reshape(-1, 1)
+            
+            jacobian_matrix = np.diagflat(single_output) - \
+                                np.dot(single_output, single_output.T)
+
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
