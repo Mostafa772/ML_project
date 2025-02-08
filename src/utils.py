a18b9fd4bbfbb36b7148b05e8ded2eed1307479d
Mostafa772 <47507394+Mostafa772@users.noreply.github.com>
2025-02-08 20:08:32 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -44,3 +44,23 @@ def plot_accuracies(train_vals, val_vals, label1="train_accuracies", label2="val
     plt.legend()
     plt.grid(True)
     plt.show()
+
+
+def plot_losses(train_vals, val_vals, label1="train_losses", label2="val_losses", title="Loss Over Epochs"):
+    """
+    Plot training and validation losses over epochs.
+
+    Parameters:
+    - train_losses (list or array): Loss values for training data over epochs.
+    - val_losses (list or array): Loss values for validation data over epochs.
+    - title (str): Title of the plot. Default is "Loss Over Epochs".
+    """
+    plt.figure(figsize=(8, 6))
+    plt.plot(train_vals, label=label1, color="blue", linewidth=2)
+    plt.plot(val_vals, label=label2, linestyle="--", color="orange", linewidth=2)
+    plt.title(title)
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
